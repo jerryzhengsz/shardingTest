315538f2e5112480463b61c8fc525b937a58360b
jerryzhengsz <jerryzhengsz@outlook.com>
2019-07-22 17:52:55 +0800

    Initial commit

---
+++ src/main/java/com/atsz/qtd/sharding/TableShardingByYearAlgorithm.java
@@ -0,0 +1,26 @@
+package com.atsz.qtd.sharding;
+
+import io.shardingsphere.api.algorithm.sharding.PreciseShardingValue;
+import io.shardingsphere.api.algorithm.sharding.standard.PreciseShardingAlgorithm;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+
+/**
+ * @author : Jerry_Zheng
+ * @pakcageName :com.atsz.qtd.sharding
+ * @project : shardingTest
+ * @date : 2019/7/22 16:05
+ * @description : TODO
+ */
+public class TableShardingByYearAlgorithm implements PreciseShardingAlgorithm<LocalDateTime> {
+
+
+    @Override
+    public String doSharding(Collection<String> availableTargetNames, PreciseShardingValue<LocalDateTime> shardingValue) {
+        String tb_name = shardingValue.getLogicTableName() + "_";
+        String year = shardingValue.getValue().format(DateTimeFormatter.ofPattern("yyyy"));
+        return tb_name + year;
+    }
+}
